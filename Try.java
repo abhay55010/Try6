630ab651ecd851596c105b20e0477fda3688a7c0
Abhay <abhaypandey55010@gmail.com>
2021-05-10 22:18:43 +0530

    from dev2 again

--- Try.java
+++ Try.java
@@ -1,6 +1,6 @@
 public class Try {
    public static void main(String[] args) {
-       System.out.println("hello");
+       System.out.println("from dev1");
 
        System.out.println("from dev2");
    } 
